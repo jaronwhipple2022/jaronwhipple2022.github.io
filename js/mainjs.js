1f66d15241727f8e0c218d44d0f8b58d9b0d805f
jaronwhipple2022 <jaronwhipple@byui.edu>
2021-01-09 23:34:37 -0700

    assignment finished

---
+++ js/mainjs.js
@@ -0,0 +1,33 @@
+let daynames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesdy",
+    "Thursday",
+    "Friday",
+    "Saturday"
+]
+let months = [
+    "January",
+    "Febuary",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
+
+let d = new Date();
+let dayName= daynames[d.getDay()];
+let realMonth = months[d.getMonth()];
+let year = d.getFullYear();
+let currentDate = dayName + " " + realMonth + " " + d.getDate() + ", " + year;
+
+document.getElementById("date").textContent = currentDate;
+
+document.log(currentDate);
